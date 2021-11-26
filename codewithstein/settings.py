4ee088998a869898f2a0a8090f0c485042f4e56f
Mnzava Heri Itika <Mnzava@avcs.local>
2021-11-26 10:35:57 +0300

    16th commit

--- codewithstein/settings.py
+++ codewithstein/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
-ALLOWED_HOSTS = ['https://soconsumerly.com','https://kagblog-g2qn9.ondigitalocean.app/']
+ALLOWED_HOSTS = ['soconsumerly.com']
 
 
 
