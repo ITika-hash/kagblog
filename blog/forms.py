bec6d2ea01edaafd3c0c72b8696552ed6b3826de
Mnzava Heri Itika <Mnzava@avcs.local>
2021-11-30 08:24:23 +0300

    Finale10

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
 from .models import Comment
+from .models import About
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
+
+class AboutForm(forms.ModelForm):
+    class Meta:
+        model = About
+        fields = ['text', 'thumb']
